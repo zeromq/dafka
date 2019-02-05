07477f8164273f4db0015bed1815a9a8a5a777df
Kevin Sapper <kevin.sapper@detim.de>
2019-02-05 18:35:43 +0100

    Problem: Out of sync with zproject
    Solution: Re-generate!

--- src/dafka_selftest.c
+++ src/dafka_selftest.c
@@ -37,11 +37,11 @@ all_tests [] = {
     { "dafka_producer", dafka_producer_test, true, true, NULL },
     { "dafka_consumer", dafka_consumer_test, true, true, NULL },
     { "dafka_proto", dafka_proto_test, true, true, NULL },
+    { "dafka_tower", dafka_tower_test, true, true, NULL },
+    { "dafka_store", dafka_store_test, true, true, NULL },
 #ifdef DAFKA_BUILD_DRAFT_API
 // Tests for draft public classes:
     { "dafka_beacon", dafka_beacon_test, false, true, NULL },
-    { "dafka_tower", dafka_tower_test, false, true, NULL },
-    { "dafka_store", dafka_store_test, false, true, NULL },
 #endif // DAFKA_BUILD_DRAFT_API
     {NULL, NULL, 0, 0, NULL}          //  Sentinel
 };
